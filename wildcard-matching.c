984846aa1a4a59709c6b13a855a3eb68a73aae66
AndesPooh258 <andespooh258@gmail.com>
2023-03-07 00:56:51 +0800

    Leetcode 44
    Two pointers
    Time: 0 ms (100.00%) Memory: 6 MB (59.70%)

---
+++ wildcard-matching.c
@@ -0,0 +1,10 @@
+bool isMatch(char* s, char* p) {
+    for (char* lastMatch = s, *lastStar = NULL; *s;) {
+        if (*p == *s || *p == '?') {p++; s++;}
+        else if (*p == '*') {lastStar = p++; lastMatch = s;}
+        else if (lastStar) {p = lastStar + 1; s = ++lastMatch;}
+        else return false;
+    }
+    for (; *p == '*'; p++);
+    return !*p;
+}
