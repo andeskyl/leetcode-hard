97cbd29e9c53246fa8f75c59d9e79d98545aedc3
AndesPooh258 <andespooh258@gmail.com>
2023-03-12 15:14:42 +0800

    Leetcode 23
    Divide and Conquer
    Time: 10 ms (99.53%), Space: 8.1 MB (92.71%)

---
+++ merge-k-sorted-lists.c
@@ -0,0 +1,18 @@
+struct ListNode* mergeTwoLists(struct ListNode* list1, struct ListNode* list2) {
+    if (!list1 && !list2) return NULL;
+    struct ListNode* head = NULL, **current = &head, **node = NULL;
+    while (list1 && list2) {
+        node = list1 -> val <= list2 -> val ? &list1 : &list2;
+        *current = *node;
+        *node = (*node) -> next;
+        current = &(*current) -> next;
+    }
+    *current = !list1 ? list2 : list1;
+    return head;
+}
+struct ListNode* mergeKLists(struct ListNode** lists, int listsSize){
+    if (!listsSize) return NULL;
+    else if (listsSize == 1) return lists[0];
+    int mid = listsSize >> 1;
+    return mergeTwoLists(mergeKLists(lists, mid), mergeKLists(lists + mid, listsSize - mid));
+}
