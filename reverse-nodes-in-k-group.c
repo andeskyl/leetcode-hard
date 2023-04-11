d19413d46d4d23dac87b51cc73f0cce721d7a05d
AndesPooh258 <andespooh258@gmail.com>
2023-04-11 17:58:40 +0800

    Leetcode 25
    Iteration
    Time: 3 ms (98.15%), Space: 6.9 MB (70.37%)

---
+++ reverse-nodes-in-k-group.c
@@ -0,0 +1,28 @@
+struct ListNode* reverseSublist(struct ListNode* head, int k) {
+    struct ListNode* newHead, *nextNode;
+    for (newHead = NULL, nextNode = NULL; k > 0; k--) {
+        nextNode = head -> next;
+        head -> next = newHead;
+        newHead = head;
+        head = nextNode;
+    }
+    return newHead;
+}
+
+struct ListNode* reverseKGroup(struct ListNode* head, int k) {
+    if (k == 1) return head;
+    struct ListNode* newHead = NULL, *lastSublistEnd = NULL, *sublistHead = head, *newSublistHead = NULL;
+    for (int count = k - 1; head != NULL; count--) {
+        head = head -> next;
+        if (!count) {
+            newSublistHead = reverseSublist(sublistHead, k);
+            if (!newHead) newHead = newSublistHead;
+            if (lastSublistEnd) lastSublistEnd -> next = newSublistHead;
+            lastSublistEnd = sublistHead;
+            sublistHead = head;
+            count = k;
+        }
+    }
+    lastSublistEnd -> next = sublistHead;
+    return newHead;
+}
