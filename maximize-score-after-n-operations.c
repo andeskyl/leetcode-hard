d4f81273268e9ddf323387e944161bb152d57e42
AndesPooh258 <andespooh258@gmail.com>
2023-05-14 15:11:01 +0800

    Leetcode 1799
    DP, Bit Manipulation
    Time: 197 ms (100.00%), Space: 6.8 MB (100.00%)

--- maximize-score-after-n-operations.c
+++ maximize-score-after-n-operations.c
@@ -2,22 +2,22 @@
 int gcdMem[14][14] = {0};
 int gcd(int a, int b) {return b == 0 ? a : gcd(b, a % b);}
 int gcdWithMem(int a, int b) {return gcdMem[a][b] > 0 ? gcdMem[a][b] : gcd(a, b);}
-int getMaxScore(int* nums, int numsSize, int* maxScoreMem, int step, unsigned short used) {
-    if (maxScoreMem[used] > 0) return maxScoreMem[used];
+int getMaxScore(int* nums, int numsSize, int* ms, int step, unsigned short used) {
+    if (ms[used] > 0) return ms[used];
     else if (step > numsSize / 2) return 0;
     for (int i = 0; i < numsSize; i++) {
         if (used & (1 << i)) continue;
         for (int j = i + 1; j < numsSize; j++) {
             if (used & (1 << j)) continue;
             unsigned short new_used = used | (1 << i) | (1 << j);
-            int res = step * gcd(nums[i], nums[j]) + getMaxScore(nums, numsSize, maxScoreMem, step + 1, new_used);
-            maxScoreMem[used] = max(maxScoreMem[used], res);
+            int res = step * gcd(nums[i], nums[j]) + getMaxScore(nums, numsSize, ms, step + 1, new_used);
+            ms[used] = max(ms[used], res);
         }
     }
-    return maxScoreMem[used];
+    return ms[used];
 }
 int maxScore(int* nums, int numsSize) {
-    int* maxScoreMem = malloc((1 << numsSize) * sizeof(int));
-    memset(maxScoreMem, 0, sizeof(maxScoreMem));
-    return getMaxScore(nums, numsSize, maxScoreMem, 1, 0);
+    int* ms = malloc((1 << numsSize) * sizeof(int));
+    memset(ms, 0, sizeof(ms));
+    return getMaxScore(nums, numsSize, ms, 1, 0);
 }
