cec8f7c838a1ff42686ade95e3e0f6ed89b87d04
AndesPooh258 <andespooh258@gmail.com>
2023-05-23 00:46:59 +0800

    Leetcode 135
    Greedy
    Time: 15 ms (100.00%), Space: 6.9 MB (100.00%)

---
+++ candy.c
@@ -0,0 +1,22 @@
+#define max(a, b) ({__typeof__ (a) _a = (a); __typeof__ (b) _b = (b); _a > _b ? _a : _b;})
+#define min(a, b) ({__typeof__ (a) _a = (a); __typeof__ (b) _b = (b); _a < _b ? _a : _b;})
+int candy(int* ratings, int ratingsSize) {
+    int up_count = 0, down_count = 0, dir = 1, res = 1;
+    for (int i = 1; i < ratingsSize; i++) {
+        if (ratings[i] > ratings[i - 1] && dir == 1) {
+            up_count++;
+        } else if (ratings[i] < ratings[i - 1]) {
+            dir = -1, down_count++;
+        } else {
+            int n = max(up_count, down_count), k = min(up_count, down_count);
+            res += (n + 2) * (n + 1) / 2 + k * (k + 1) / 2 - 1;
+            if (ratings[i] == ratings[i - 1]) {
+                up_count = 0, res++;
+            } else up_count = 1;
+            down_count = 0, dir = 1;
+        }
+    }
+    int n = max(up_count, down_count), k = min(up_count, down_count);
+    res += (n + 2) * (n + 1) / 2 + k * (k + 1) / 2 - 1;
+    return res;
+}
